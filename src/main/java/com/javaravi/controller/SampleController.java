325c604979b83df91ff0bdaee3a38a691fe37223
javaravikiran <ravikiran.namana@gmail.com>
2021-09-15 02:02:18 +0530

    initial commit

---
+++ src/main/java/com/javaravi/controller/SampleController.java
@@ -0,0 +1,19 @@
+package com.javaravi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/springdemo")
+public class SampleController {
+	
+	
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome To Spring Boot :";
+	}
+
+}
