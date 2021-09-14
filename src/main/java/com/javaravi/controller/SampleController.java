c63bd33a9e61a3460bd022df0bd7fb317d0fd465
javaravikiran <ravikiran.namana@gmail.com>
2021-09-15 02:06:32 +0530

    first commit

--- src/main/java/com/javaravi/controller/SampleController.java
+++ src/main/java/com/javaravi/controller/SampleController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/springdemo")
-public class SampleController {
-	
-	
+public class SampleController {	
 	
 	@GetMapping("/welcome")
 	public String welcome() {
